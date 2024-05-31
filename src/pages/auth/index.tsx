074be6817f63fb23f2af116d385588dbbc9fccbc
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-05-31 13:16:03 +0530

    create and set all dependecies and packages

---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type AuthPageProps = {
+    
+};
+
+const AuthPage:React.FC<AuthPageProps> = () => {
+    
+    return <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
+        <div className='max-w-7xl mx-auto'>
+            
+        </div>
+    </div>
+}
+export default AuthPage;
